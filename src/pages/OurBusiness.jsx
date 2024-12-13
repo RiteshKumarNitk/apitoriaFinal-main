debc1801b332acc86d135b3b0a846a48d420c7f6
Ritesh Kumar <rav332211@gmail.com>
2024-12-13 17:53:45 +0530

    our business page updated

--- src/pages/OurBusiness.jsx
+++ src/pages/OurBusiness.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 
 const OurBusiness = () => {
   return (
-    <div>
-      OurBusiness
-    </div>
+    <div className=" xl:mt-24 lg:mt-24 md:mt-32 sm:mt-24 mt-8">
+      <p>je;p</p>
+      <p>je;p</p>
+      <p>je;p</p>
+  </div>
   )
 }
 
